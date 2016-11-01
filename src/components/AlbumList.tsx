210cc4481661c8602f031710a37375988080d37c
Matt Hackmann <matt@dxprog.com>
2016-10-31 22:39:37 -0700

    it plays songs. we're done!

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -10,13 +10,22 @@ export interface ArrayListProps {
   songs: Array<Song>;
 }
 
+function songClick(evt: MouseEvent) {
+  const audio = document.createElement('audio');
+  audio.addEventListener('canplaythrough', evt => {
+    audio.play();
+  });
+  audio.src = `http://dxmp.s3.amazonaws.com/songs/${this.filename}`;
+  audio.load();
+}
+
 export class AlbumList extends React.Component<ArrayListProps, {}> {
   render() {
-    return ( 
-      <ul key="album-${this.props.album.id}">
+    return (
+      <ul key={`album-${this.props.album.id}`}>
         <ListImageItem imageUrl={this.props.album.artUrl} title={this.props.album.title} id={this.props.album.id} />
         <ul>
-          {this.props.songs.filter(song => song.album_id === this.props.album.id).map(song => <ListSimpleItem title={song.title} id={song.id} />)}
+          {this.props.songs.filter(song => song.album_id === this.props.album.id).map(song => <ListSimpleItem title={song.title} id={song.id} onClick={songClick.bind(song)} />)}
         </ul>
       </ul>
     );
--- src/components/ListSimpleItem.tsx
+++ src/components/ListSimpleItem.tsx
@@ -3,12 +3,13 @@ import * as React from 'react';
 export interface ListSimpleItemProps {
   id: number;
   title: string;
+  onClick: Function;
 }
 
 export class ListSimpleItem extends React.Component<ListSimpleItemProps, {}> {
   render() {
     return (
-      <li key={this.props.id}>
+      <li key={this.props.id} onClick={this.props.onClick.bind(this)}>
         <h3>{this.props.title}</h3>
       </li>
     );
