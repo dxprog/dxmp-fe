f5b30910e449df25f4d44b3eac62da5f2d377b9e
Matt Hackmann <matt@dxprog.com>
2017-04-15 14:42:07 -0700

    full unit tests for Eventable

--- tests/interfaces/eventable.spec.ts
+++ tests/interfaces/eventable.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import 'sinon';
 
 import Eventable from '../../src/interfaces/eventable';
 
@@ -6,8 +7,43 @@ import Eventable from '../../src/interfaces/eventable';
 class EventableTest extends Eventable {}
 
 describe('Eventable tests', () => {
+  let testInstance: EventableTest;
+  let sandbox: sinon.SinonSandbox;
+
+  beforeEach(() => {
+    testInstance = new EventableTest();
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    testInstance = null;
+    sandbox.restore();
+  });
+
   it('should add the "on" and "fire" methods', () => {
-    const testInstance = new EventableTest();
     expect(testInstance.on).to.be.a('function');
+    expect(testInstance.fire).to.be.a('function');
+  });
+
+  it('should call added event handler only when that event is fired', () => {
+    const MY_EVENT = 'MY_EVENT';
+    const eventSpy = sandbox.spy();
+    testInstance.on(MY_EVENT, eventSpy);
+    testInstance.fire(MY_EVENT);
+    testInstance.fire('some-other-event');
+    sinon.assert.calledOnce(eventSpy);
+  });
+
+  it('should invoke multiple event listeners in the order they were added', () => {
+    const MY_EVENT = 'MY_EVENT';
+    const firstEventSpy = sandbox.spy(() => {
+      sinon.assert.notCalled(secondEventSpy);
+    });
+    const secondEventSpy = sandbox.spy(() => {
+      sinon.assert.calledOnce(firstEventSpy);
+    });
+    testInstance.on(MY_EVENT, firstEventSpy);
+    testInstance.on(MY_EVENT, secondEventSpy);
+    testInstance.fire(MY_EVENT);
   });
 });
