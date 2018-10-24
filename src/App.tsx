2c3ccf20d466f034ec8692999d84dd8c5cf72c42
cphackm <cphackm@gmail.com>
2018-10-23 23:11:03 -0700

    Add basic search function (#12)
    * Added main layout
    
    * Added control and art panes.
    
    * Nits
    
    * Break list pane into 3-column table
    
    * Open search pane when pressing key.
    
    * Added basic search function.

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,35 @@ import { ArtPane } from './components/ArtPane';
 import { ControlPane } from './components/ControlPane';
 import * as xhr from './lib/xhr';
 
-import { IAlbum } from 'dxmp-common';
+import { IAlbum, ISong } from 'dxmp-common';
 import * as React from 'react';
 
 interface IState {
   albums: IAlbum[],
   expandInterface: boolean,
+  songs: ISong[],
 };
 
 class App extends React.Component<{}, IState> {
   public state: IState = {
     albums: [], 
     expandInterface: false,
+    songs: [],
   };
 
   public async componentDidMount(): Promise<any> {
-    const albums = await xhr.request('http://api.dxmp.us/albums');
-    this.setState(albums);
+    const [albums, songs] = await Promise.all([
+      xhr.request('http://api.dxmp.us/albums'),
+      xhr.request('http://api.dxmp.us/songs'),
+    ]);
+    this.setState({albums: albums as IAlbum[], songs: songs as ISong[]});
   }
 
   public render() {
     return (
       <div className="App">
         <ArtPane />
-        <ControlPane albums={this.state.albums} />
+        <ControlPane albums={this.state.albums} songs={this.state.songs}/>
       </div>
     );
   }
--- src/components/ListPane.tsx
+++ src/components/ListPane.tsx
@@ -1,42 +1,71 @@
-import { IAlbum } from 'dxmp-common';
+import { IAlbum, ISong } from 'dxmp-common';
 import * as React from 'react';
 
 import './ListPane.scss';
 
 interface IListPaneProps {
   albums: IAlbum[],
+  songs: ISong[],
 };
 
 interface IListPaneState {
   query: string,
+  queryResults: ISong[],
 };
 
-export class ListPane extends React.Component<IListPaneProps> {
+export class ListPane extends React.Component<IListPaneProps, IListPaneState> {
   public state: IListPaneState = {
     query: '',
+    queryResults: [],
   };
 
   public render(): React.ReactNode {
     return (
-      <table className="list-pane">
-        <tbody>
-          <tr>
-            <td className="meta-list"/>
-            <td className="results-list">
-              <input 
-                autoFocus={true} 
-                value={this.state.query} 
-                onChange={this.onQueryChange}
-              />
-            </td>
-            <td className="queue"/>
-          </tr>
-        </tbody>
-      </table>
+      <div className="list-pane">
+        <div className="meta-list"/>
+        <div className="results-pane">
+          <input 
+            autoFocus={true} 
+            value={this.state.query} 
+            onChange={this.onQueryChange}
+          />
+          {this.renderQueryResults()}
+        </div>
+        <div className="queue"/>
+      </div>
+    );
+  }
+
+  private renderQueryResults(): React.ReactNode {
+    return (
+      <ul className="results-list">
+        {this.state.queryResults.map(
+          song => {
+            const {query} = this.state;
+            const {title} = song;
+            const queryPos = title.toLowerCase().indexOf(query);
+            const prefix = title.substr(0, queryPos);
+            const match = title.substr(queryPos, query.length);
+            const suffix = title.substr(queryPos + query.length);
+            return (
+              <li className="result" key={song.id}>
+                {prefix}<span className="highlight">{match}</span>{suffix}
+              </li>
+            );
+          },
+        )}
+      </ul>
     );
   }
 
   private onQueryChange = (event: React.FormEvent<HTMLInputElement>): void => {
-    this.setState({query: event.currentTarget.value});
+    const query = event.currentTarget.value.toLowerCase();
+    const querySet = this.state.query !== '' && query.startsWith(this.state.query) 
+      ? this.state.queryResults 
+      : this.props.songs;
+    const queryResults = querySet.filter(
+      song => song.title.toLowerCase().includes(query),
+    );
+    this.setState({query, queryResults});
   }
 }
